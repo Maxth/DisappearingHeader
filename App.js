8311d32760b916f71f1851c6c1343794f9fcef88
maxth <maxthorstorp@gmail.com>
2023-02-27 12:22:54 +0100

    first commit

---
+++ App.js
@@ -0,0 +1,93 @@
+import React, {useState, useEffect} from 'react';
+
+import {StyleSheet, View, Text, StatusBar, ScrollView} from 'react-native';
+import {useHeaderHeight} from '@react-navigation/elements';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
+
+const RootStack = createNativeStackNavigator();
+const Tab = createMaterialTopTabNavigator();
+const Stack = createNativeStackNavigator();
+
+const StartScreen = () => <View />;
+
+const Screen = () => {
+  const headerHeight = useHeaderHeight();
+  console.log(headerHeight);
+  return (
+    <View style={styles.screen}>
+      <StatusBar />
+      <Text>Content</Text>
+      <ScrollView showsVerticalScrollIndicator={false} bounces={false}>
+        <View
+          style={{
+            height: 300 + Math.random() * 500,
+            width: 100,
+            backgroundColor: 'blue',
+          }}
+        />
+      </ScrollView>
+    </View>
+  );
+};
+
+const defaultStackOptions = {
+  headerStyle: {
+    backgroundColor: 'red',
+  },
+  headerTitleStyle: {
+    color: 'white',
+    fontWeight: '800',
+  },
+  headerTitleAlign: 'center',
+  animation: 'slide_from_right',
+  gestureEnabled: false,
+  orientation: 'portrait_up',
+};
+
+const defaultTabBarStyle = {
+  tabBarStyle: {height: 100, backgroundColor: 'red'},
+};
+
+const BottomStack = () => (
+  <Stack.Navigator screenOptions={defaultStackOptions}>
+    <Stack.Screen name="stack" options={{title: 'Header'}} component={Screen} />
+  </Stack.Navigator>
+);
+
+const TopTab = () => (
+  <Tab.Navigator screenOptions={defaultTabBarStyle} tabBarPosition="bottom">
+    <Tab.Screen name="tab" component={BottomStack} />
+  </Tab.Navigator>
+);
+
+const App = () => {
+  const [showStart, setShowStart] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setShowStart(false);
+    }, 500);
+  }, []);
+
+  return (
+    <NavigationContainer>
+      <RootStack.Navigator
+        initialRouteName=""
+        screenOptions={{headerShown: false}}>
+        {showStart ? (
+          <RootStack.Screen name="start" component={StartScreen} />
+        ) : (
+          <RootStack.Screen name="root" component={TopTab} />
+        )}
+      </RootStack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {flex: 1, justifyContent: 'center', alignItems: 'center'},
+});
+
+export default App;
